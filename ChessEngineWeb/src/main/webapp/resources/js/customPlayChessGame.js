018c8633f0295263ce09fd7a871618e6f575d8f3
David Balazs <david.balazs@iquestgroup.com>
2016-03-29 15:49:08 +0300

    move works

--- ChessEngineWeb/src/main/webapp/resources/js/customPlayChessGame.js
+++ ChessEngineWeb/src/main/webapp/resources/js/customPlayChessGame.js
@@ -5,15 +5,13 @@ var board,
     pgnEl = $('#pgn');
 
 // do not pick up pieces if the game is over
-// only pick up pieces for the side to move
+// only pick up pieces for White
 var onDragStart = function(source, piece, position, orientation) {
-    if (game.game_over() === true ||
-        (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+    if (game.in_checkmate() === true || game.in_draw() === true ||
+        piece.search(/^b/) !== -1) {
         return false;
     }
 };
-
 
 var onDrop = function (source, target) {
     // see if the move is legal
@@ -39,7 +37,7 @@ var onSnapEnd = function () {
 
 function makeAjaxCallForNextMove() {
     $.ajax({
-        url: "http://localhost:9090/playgame/next-move?chessPositionFen=" + board.fen() + "&sideToMove=BLACK&virtualPlayerLevel=1",
+        url: "http://localhost:9090/playgame/next-move?chessPositionFen=" + board.fen() + "&sideToMove=BLACK&virtualPlayerLevel=4",
         type: "GET",
         success: function (move) {
             game.move({
