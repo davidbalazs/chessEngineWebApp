5527de01587bb6dc3bff7e3fd75095a95ea702ac
David Balazs <david.balazs@iquestgroup.com>
2016-04-11 13:33:53 +0300

    added persistence support

--- ChessEngineCore/src/main/java/com/davidbalazs/chess/models/LatestNewsEntityModel.java
+++ ChessEngineCore/src/main/java/com/davidbalazs/chess/models/LatestNewsEntityModel.java
@@ -1,14 +1,30 @@
 package com.davidbalazs.chess.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 
 /**
  * Created by David on 4/5/2016.
  */
+@Entity
+@Table(name = "LATEST_NEWS")
 public class LatestNewsEntityModel {
+    @Id
+    @Column(name="ID")
+    @GeneratedValue
     private long id;
+
+    @Column(name="TITLE")
     private String title;
+
+    @Column(name="TEXT")
     private String text;
+
+    @Column(name="DATE")
     private Date date;
 
     public long getId() {
---
+++ ChessEngineWeb/src/main/java/com/davidbalazs/chess/controllers/MainPageController.java
@@ -0,0 +1,28 @@
+package com.davidbalazs.chess.controllers;
+
+import com.davidbalazs.chess.controllers.enhancers.MainPageEnhancer;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.Resource;
+
+/**
+ * @author: david.balazs@iquestgroup.com
+ */
+@Controller
+public class MainPageController {
+    public static final Logger LOGGER = Logger.getLogger(MainPageController.class);
+
+    @Resource(name = "mainPageEnhancer")
+    private MainPageEnhancer mainPageEnhancer;
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String loadHomePage(Model model) {
+        mainPageEnhancer.enhanceModelWithSideBar(model);
+        return "pages/homePage";
+    }
+}
