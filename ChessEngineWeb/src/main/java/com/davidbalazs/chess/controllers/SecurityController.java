3323ce3ab22a1ce0178ebeb623595214d5c2620c
David Balazs <david.balazs@iquestgroup.com>
2016-05-04 09:45:28 +0300

    fixed logout functionality

--- ChessEngineWeb/src/main/java/com/davidbalazs/chess/controllers/SecurityController.java
+++ ChessEngineWeb/src/main/java/com/davidbalazs/chess/controllers/SecurityController.java
@@ -1,18 +1,39 @@
 package com.davidbalazs.chess.controllers;
 
+import org.apache.log4j.Logger;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.security.Principal;
+import java.text.MessageFormat;
 
 /**
  * @author: david.balazs@iquestgroup.com
  */
 @Controller
 public class SecurityController {
+    public static final Logger LOGGER = Logger.getLogger(ContactUsPageController.class);
+    private static final String LOGOUT_LOG_MESSAGE = "Received request to logout user with name : {0}";
 
     @RequestMapping(value = "/access-denied", method = RequestMethod.GET)
     public String accessDeniedPage(Model model) {
         return "pages/security/accessDeniedPage";
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response, Principal principal) {
+        LOGGER.info(MessageFormat.format(LOGOUT_LOG_MESSAGE, principal.getName()));
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "pages/homePage";
     }
 }
--- ChessEngineWeb/src/main/webapp/WEB-INF/views/pages/administration/mainAdministrationPage.jsp
+++ ChessEngineWeb/src/main/webapp/WEB-INF/views/pages/administration/mainAdministrationPage.jsp
@@ -7,9 +7,7 @@
 <body>
 <h1>Admin page</h1>
 <c:url value="/logout" var="logoutUrl"/>
-<form id="logout" action="${logoutUrl}" method="post">
-    <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}" />
-    <button type="submit" value="logout" name="logout"/>
+<a href="${logoutUrl}">Logout</a>
 </form>
 </body>
 </html>
